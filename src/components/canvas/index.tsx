81e20d6b9fc36ef8d3fd42a620a06c6ac4626b0e
Ujjwal Sharma <ujjwal.sharma@dhiway.com>
2023-07-15 15:09:54 +0530

    revert back to original setup

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -1,5 +1,5 @@
-import ShapesHandler from "../shape/index";
-import { useImageStore } from "../../store/index";
+import ShapesHandler from "../shape";
+import { useImageStore } from "../../store";
 const CanvasSection = () => {
   const { imageData } = useImageStore();
   return (
